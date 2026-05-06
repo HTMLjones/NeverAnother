cb894c8e5727313325892562275cec5e899adb80
Simon <junkersje@gmail.com>
2026-05-06 10:48:18 +0200

    Pull request test1

--- app/src/main/java/com/example/neveranother/MainActivity.kt
+++ app/src/main/java/com/example/neveranother/MainActivity.kt
@@ -5,26 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.neveranother.navigation.AppNavigation
-import com.example.neveranother.pages.test
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            class MainActivity : ComponentActivity() {
-                override fun onCreate(savedInstanceState: Bundle?) {
-                    super.onCreate(savedInstanceState)
-                    setContent {
-                        val viewModel: ViewModel = viewModel()
-                        MaterialTheme {
-                            AppNavigation()
-                        }
-                    }
-                }
+            MaterialTheme {
+                AppNavigation()
             }
         }
     }
