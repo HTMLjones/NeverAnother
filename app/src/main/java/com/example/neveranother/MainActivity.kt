37b855e215b695269e4b410974371bc4e5a09f42
jonas <joni0014@stud.ek.dk>
2026-05-06 11:04:34 +0200

    Hey comment

--- app/src/main/java/com/example/neveranother/MainActivity.kt
+++ app/src/main/java/com/example/neveranother/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
                     super.onCreate(savedInstanceState)
                     setContent {
                         //hej
-
+                        //Hey
                         // hola :)
                         MaterialTheme {
                             AppNavigation()
