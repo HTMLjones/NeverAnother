a03a6b865766f30b2aafd0ff24c1c1e48e4e4c15
HTMLjones <joni0014@stud.ek.dk>
2026-05-27 22:43:16 +0200

    Added tests

--- app/src/test/java/com/example/neveranother/ExampleUnitTest.kt
+++ app/src/test/java/com/example/neveranother/ExampleUnitTest.kt
@@ -3,9 +3,11 @@ package com.example.neveranother
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
-import com.example.neveranother.pages.Homescreen
-import com.example.neveranother.classes.viewModel.neverAnotherViewModel
+import com.example.neveranother.classes.viewModel.ProfilViewModel
+import com.example.neveranother.pages.LoginScreen
+import com.example.neveranother.pages.ProfileStartScreen
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -20,12 +22,41 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
     @Test
     fun isBuyOnScreen() {
         val composeTestRule = createComposeRule()
         composeTestRule.setContent {
-            Homescreen(navController = rememberNavController(), viewModel = neverAnotherViewModel())
+            Homescreen(navController = rememberNavController(), viewModel = ProfilViewModel())
         }
         composeTestRule.onNodeWithText("Buy").assertIsDisplayed()
+    }
+
+
+
+    //Jonas Mørk Nielsen
+    @Test
+    fun isLoginOptionOnScreen() {
+        val composeTestRule = createComposeRule()
+        composeTestRule.setContent {
+            ProfileStartScreen(
+                navController = rememberNavController(),
+                viewModel = ProfilViewModel()
+            )
+        }
+        composeTestRule.onNodeWithText("Login").assertIsDisplayed()
+    }
+
+
+    @Test
+    fun isInputAdgangskodeOnScreen() {
+        val composeTestRule = createComposeRule()
+        composeTestRule.setContent {
+            LoginScreen(
+                navController = rememberNavController(),
+                viewModel = ProfilViewModel()
+            )
+        }
+        composeTestRule.onNodeWithText("Adgangskode").assertIsDisplayed()
     }
 }
